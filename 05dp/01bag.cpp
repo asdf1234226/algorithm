16cb674a19357d13f9ee26f94019a3905c527071
asdf1234226 <2432628481@qq.com>
2023-10-23 03:38:46 -0400

    init dp

---
+++ 05dp/01bag.cpp
@@ -0,0 +1,23 @@
+//  https://www.acwing.com/problem/content/2/
+// 0<n,m≤1000
+// 0<vi,wi≤100
+
+#include<iostream>
+using namespace std;
+int n; //物品个数
+int v; //背包容量
+const int N = 1010;
+int w[N];//价值
+int v[N];//体积
+
+int max_value(){
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+    
+}
+
+int main(){
+    return 0;
+}
