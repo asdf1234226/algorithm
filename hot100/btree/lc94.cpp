ffbd812da670ee9966a09f4622dcf40f49af51ca
asdf1234226 <2432628481@qq.com>
2023-11-12 09:49:19 -0500

    b tree example

--- hot100/btree/lc94.cpp
+++ hot100/btree/lc94.cpp
@@ -2,15 +2,47 @@
 // Created by SP on 2023/11/12.
 //
 
-*struct TreeNode {
-    *     int val;
-    *     TreeNode *left;
-    *     TreeNode *right;
-    *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    * };
+#include <vector>
+#include<iostream>
+using namespace std;
 
-vector<int> inorderTraversal(TreeNode* root) {
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
+void helper(TreeNode* node, vector<int>& res) {
+    if (node != nullptr) {
+        if (node->left != nullptr) {
+            helper(node->left, res);
+        }
+        res.push_back(node->val);
+        if (node->right != nullptr) {
+            helper(node->right, res);
+        }
+    }
 }
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    helper(root, res);
+    return res;
+}
+    
+
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+
+    vector<int> res = inorderTraversal(root);
+    for (int i : res) {
+        cout << i << " ";
+    }
+    return 0;
+}
+
