581543eacdd28a497df73bb2af7c6458a7b47429
asdf1234226 <2432628481@qq.com>
2023-11-23 21:25:38 +0800

    1123

--- hot100/btree/lc101.cpp
+++ hot100/btree/lc101.cpp
@@ -1,10 +1,55 @@
 //
 // Created by SP on 2023/11/12.
 //
-给你一个二叉树的根节点 root ， 检查它是否轴对称
-输入：root = [1,2,2,3,4,4,3]
-输出：true
+//给你一个二叉树的根节点 root ， 检查它是否轴对称
+//输入：root = [1,2,2,3,4,4,3]
+//输出：true
 
+
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x){
+        val=x;
+        left= nullptr;
+        right= nullptr;
+    }
+};
+
+
+bool compare1(TreeNode* left, TreeNode* right){
+    if (left== nullptr && right== nullptr) return true;
+    else if (left!= nullptr || right != nullptr) return false;
+    else if (left->val != right->val) return false;
+    bool outside = compare1(left->left, right->right);
+    bool inside = compare1(left->right, right->left);
+    return outside&&inside;
+}
+//左右节点都空         true
+//一空一不空 或 两个不同       false
+//相同时，还要看下一层的值是否对称，内外侧都要相同；外侧指左的左和右的右；内测指左的右和右的左
 bool isSymmetric(TreeNode* root) {
+    if (root== nullptr) return true;
+    return compare1(root->left, root->right);
+}
+
+int main(){
+    TreeNode* root = new TreeNode(1);
+    root->left=new TreeNode(2);
+    root->right=new TreeNode(2);
+    root->left->left=new TreeNode(3);
+    root->left->right=new TreeNode(4);
+    root->right->left=new TreeNode(4);
+    root->right->right=new TreeNode(3);
+    cout << isSymmetric(root);
+    return 0;
 
 }
--- hot100/btree/lc104.cpp
+++ hot100/btree/lc104.cpp
@@ -5,8 +5,38 @@
 //给定一个二叉树 root ，返回其最大深度
 //输入：root = [3,9,20,null,null,15,7]
 //输出：3
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
 
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x){
+        val = x;
+        left=nullptr;
+        right=nullptr;
+    }
+};
 
-int maxDepth(TreeNode* root) {
+//1. 后续求高度
+//2. 层序遍历
+int maxDepth(TreeNode* root) {//根节点的最大高度，后序求的是高度
+    if (root== nullptr) return 0;
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return max(left,right)+1;
+}
 
+int main(){
+    TreeNode* root = new TreeNode(3);
+    root->left=new TreeNode(9);
+    root->right=new TreeNode(20);
+    root->right->left=new TreeNode(15);
+    root->right->right=new TreeNode(7);
+    cout << maxDepth(root);
+    return 0;
 }
