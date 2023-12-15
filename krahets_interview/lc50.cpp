eb49ae5923a1bc470ba594ad0888fe49387f3fb6
asdf1234226 <2432628481@qq.com>
2023-12-15 05:07:47 -0500

    1215

--- krahets_interview/lc50.cpp
+++ krahets_interview/lc50.cpp
@@ -1,11 +1,53 @@
+// 实现 pow(x, n) ，即计算 x 的整数 n 次幂函数（即，xn ）。
 
-实现 pow(x, n) ，即计算 x 的整数 n 次幂函数（即，xn ）。
+// 输入：x = 2.10000, n = 3
+// 输出：9.26100
+// 输入：x = 2.00000, n = -2
+// 输出：0.25000
 
-输入：x = 2.10000, n = 3
-输出：9.26100
-输入：x = 2.00000, n = -2
-输出：0.25000
-
+#include<iostream>
+using namespace std;
+//3=2^0+2^1
+//11=2^0+2^1+2^3
 double myPow(double x, int n) {
-
+    if (n<0)//幂指数为负数的情况
+    {
+        n=-n;
+        x=1/x;
+    }
+    double res = 1.0;
+//    1. 当 n&1=1 时：将当前 x 乘入 res。
+//    2. 执行x=x^2。
+//    3. n右移。
+    while (n>0)
+    {
+        if (n&1==1)
+        {
+            res*=x;
+        }
+        x*=x;
+        n>>=1;
+    }
+    return res;
 }
+
+int main(){
+    cout << myPow(2.10000,3);
+    return 0;
+}
+
+    // public double myPow(double x, int n) {
+    //     if(x == 0.0f) return 0.0d;
+    //     long b = n;
+    //     double res = 1.0;
+    //     if(b < 0) {
+    //         x = 1 / x;
+    //         b = -b;
+    //     }
+    //     while(b > 0) {
+    //         if((b & 1) == 1) res *= x;
+    //         x *= x;
+    //         b >>= 1;
+    //     }
+    //     return res;
+    // }
--- krahets_interview/lc59.cpp
+++ krahets_interview/lc59.cpp
@@ -6,6 +6,65 @@
 //输出：[[1,2,3],[8,9,4],[7,6,5]]
 //给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
 
-vector<vector<int>> generateMatrix(int n) {
+#include<iostream>
+#include<vector>
+using namespace std;
 
+vector<vector<int>> generateMatrix(int n) {
+    vector<vector<int>> res(n,vector<int>(n,0));
+    int index = 1;
+    int up = 0, down=n-1, left=0,right=n-1;
+    while (true)
+    {
+        for (int j = left; j <=right; j++)//第一行
+        {
+            res[up][j]=index++;
+        }
+        up++;
+        if (up>down)
+        {
+            break;
+        }
+        
+        for (int i = up; i <= down; i++)//右边列
+        {
+            res[i][right]=index++;
+        }
+        right--;
+        if (left>right)
+        {
+            break;
+        }
+        
+        for(int j=right;j>=left;j--)//下边行
+        {
+            res[down][j]=index++;
+        }
+        down--;
+        if (up>down)
+        {
+            break;
+        }
+        for (int i = down; i >= up; i--)//左边行
+        {
+            res[i][left]=index++;
+        }
+        left++;
+        if (left>right)
+        {
+            break;
+        }
+    }
+    return res;
+}
+
+int main(){
+    vector<vector<int>> a = generateMatrix(3);
+    for(int i=0;i<a.size();i++){
+        for(int j=0;j<a[i].size();j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
